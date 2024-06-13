d98b1e19891734d7f1cb15f5c7f0cd4495a90866
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2024-06-13 22:36:00 +0200

    added support to pipx installer

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'ADcheck=ADcheck:main',
+            'ADcheck=adcheck.app:main',
         ]
     }
 )

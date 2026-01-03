01f2db0c0b4da37a57d23474fbb690bb6dcc4aca
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2026-01-03 08:04:20 +0100

    Update winacl to the latest version after accepting the PR

--- src/adcheck/libs/winacl/winacl/_version.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.1.9"
-__banner__ = \
-"""
-# winacl %s 
-# Author: Tamas Jos @skelsec (info@skelsecprojects.com)
-""" % __version__
--- src/adcheck/libs/winacl/winacl/examples/userinfo.py
+++
@@ -1,7 +0,0 @@
-from winacl.functions.highlevel import get_logon_info
-
-def main():
-    print(get_logon_info())
-
-if __name__ == '__main__':
-    main()
